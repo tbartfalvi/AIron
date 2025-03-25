0fe8d6e7fb46edf554b3ccbb1c4dde2fa14bd9a1
tbartfalvi79 <98202640+tbartfalvi@users.noreply.github.com>
2025-03-25 11:03:30 -0700

    Removing database project into its own repositoru

--- airondata/models/user.py
+++
@@ -1,6 +0,0 @@
-# The user data object
-class User:
-    def __init__(self, full_name, email, password):
-        self.full_name = full_name
-        self.email = email
-        self.password = password
